ba9b2a84627e121ef459a8f9eed13d35d502ca27
weimin1992 <weimin1992@outlook.com>
2019-05-14 22:11:01 +0800

    layout初始化

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { Button } from 'antd';
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <Button type="primary">Button</Button>
-    </div>
-  );
-}
+const { Header, Footer, Content } = Layout;
 
+class App extends Component {
+  render() {
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu mode="horizontal" theme="dark"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">主页</Menu.Item>
+            <Menu.Item key="2">关于</Menu.Item>
+            <Menu.Item key="3">联系我们</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>主页</Breadcrumb.Item>
+            {/* <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item> */}
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>内容</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>浪民 ©2019 </Footer>
+      </Layout>
+    );
+  }
+}
 export default App;

a1957dc73d1a937a39cd5215ed18f0ed9babd5c5
weimin1992 <weimin1992@outlook.com>
2019-06-26 21:52:59 +0800

    更新antd

--- react-app/src/Home/index.js
+++ react-app/src/Home/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout,Card } from 'antd';
 import './index.css';
 const { Content } = Layout;
+const { Meta } = Card;
 class Home extends Component {
     render() {
         return (
             <Content style={{ padding: '0 50px',minHeight: 280 }}>
-                
+                <Card hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Europe Street beat" description="www.instagram.com" />
+                </Card>
             </Content>
         );
     }

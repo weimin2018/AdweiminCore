1dbbc7051f1913b4bb10bc8b402d43ef47aaf9d9
weimin1992 <weimin1992@outlook.com>
2019-07-04 21:48:40 +0800

    更新antd

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import zhCN from 'antd/lib/locale-provider/zh_CN';
-import { LocaleProvider,Layout, Menu,Icon } from 'antd';
+import { LocaleProvider,Layout, Menu,Icon,Switch } from 'antd';
 import Home from './Home';
 import './App.css';
 
 const { Header, Footer } = Layout;
 
-class App extends Component {  
+class App extends Component {
+  state = {
+    theme: 'dark',
+    current: '1',
+  };
+
+  changeTheme = value => {
+    this.setState({
+      theme: value ? 'dark' : 'light',
+    });
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+
   render() {
-    let currentDate = new Date();
-    const themeAndBgColor =currentDate.getHours()>17?['dark','#001529']:['light','#fff'];
+    const bgColor=this.state.theme ==='dark'?'#001529':'#fff';
     return (
       <LocaleProvider locale={zhCN}>
         <Layout className="layout">
-          <Header style={{ position: 'fixed', zIndex: 1, width: '100%',height:'52px',background:themeAndBgColor[1] }}>
-            <div className="logo" />
-              <Menu mode="horizontal" theme={themeAndBgColor[0]} defaultSelectedKeys={['1']} style={{ lineHeight: '52px' }} >
+            <Header style={{ position: 'fixed', zIndex: 1, width: '100%',height:'52px',backgroundColor:bgColor}} theme={this.state.theme}>
+              <Menu mode="horizontal" theme={this.state.theme} defaultSelectedKeys={['1']} style={{ lineHeight: '52px' }} >
                 <Menu.Item key="1"><Icon type="home" theme="filled" />首页</Menu.Item>
                 <Menu.Item key="2"><Icon type="eye" theme="filled" />发现</Menu.Item>
+                <Menu.Item key="9" disabled>
+                  <Switch checked={this.state.theme === 'dark'} onChange={this.changeTheme} checkedChildren="暗" unCheckedChildren="明" />
+                </Menu.Item>
               </Menu>
-          </Header>
-          <Home />
-          <Footer style={{ textAlign: 'center' }}>浪民 ©2019 </Footer>
+            </Header>
+            <Home  />
+            <Footer style={{ textAlign: 'center' }}>浪民 ©2019 </Footer>
         </Layout>
       </LocaleProvider>
     );
--- react-app/src/Home/index.js
+++ react-app/src/Home/index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { Layout,Card } from 'antd';
+import { Layout,Card ,Icon} from 'antd';
 import './index.css';
 const { Content } = Layout;
 const { Meta } = Card;
 class Home extends Component {
     render() {
         return (
-            <Content style={{ padding: '0 50px',minHeight: 280 }}>
-                <Card hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                >
-                    <Meta title="Europe Street beat" description="www.instagram.com" />
+            <Content style={{ padding: '0 50px',minHeight: 280,marginTop:180 }}>
+                <Card hoverable  style={{ width: 240 }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} 
+                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
                 </Card>
             </Content>
         );

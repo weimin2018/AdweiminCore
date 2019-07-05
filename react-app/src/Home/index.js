8b6d67ad4df3be0c488e6730a701234584612111
weimin1992 <weimin1992@outlook.com>
2019-07-05 08:10:14 +0800

    暂存

--- react-app/src/Home/index.js
+++ react-app/src/Home/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Layout,Card ,Icon} from 'antd';
 import './index.css';
-const { Content } = Layout;
-const { Meta } = Card;
+import Chart from '../img/chart.svg';
+
 class Home extends Component {
     render() {
+        const { Content } = Layout;
+        const { Meta } = Card;
         return (
-            <Content style={{ padding: '0 50px',minHeight: 280,marginTop:180 }}>
+            <Content style={{ padding: '0 50px',minHeight: 280,marginTop:80 }}>
                 <Card hoverable  style={{ width: 240 }}
-                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} 
+                    cover={<img alt="example" src={Chart} />} 
                     actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
-                    {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
+                    <Meta title="Chart" description="图表示例" />
                 </Card>
             </Content>
         );

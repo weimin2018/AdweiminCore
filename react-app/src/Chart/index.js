e9ccf28840563c208e4e07db585bddece2a8d1a8
weimin2018 <liuweimin@china-ccw.com>
2019-07-06 11:08:24 +0800

    暂存

---
+++ react-app/src/Chart/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+
+class Chart extends Component {
+    render() {
+        return (
+            <div>This is Chart Dom!</div>
+        );
+    }
+}
+export default Chart;
--- react-app/src/Home/index.js
+++ react-app/src/Home/index.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import { Layout,Card ,Icon} from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Layout,Card,Icon,Row,Col} from 'antd';
 import './index.css';
-import Chart from '../img/chart.svg';
+import ChartSvg from '../img/area chart.svg';
+import MapSvg from '../img/wan_map.svg';
+import TableSvg from '../img/table.svg';
 
 class Home extends Component {
+
+    handleApi(type){
+        console.log('type:',type);
+    }
+
     render() {
         const { Content } = Layout;
         const { Meta } = Card;
         return (
             <Content style={{ padding: '0 50px',minHeight: 280,marginTop:80 }}>
-                <Card hoverable  style={{ width: 240 }}
-                    cover={<img alt="example" src={Chart} />} 
-                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
-                    <Meta title="Chart" description="图表示例" />
-                </Card>
+                
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Card hoverable  style={{ width: 240 }}
+                                cover={<img alt="example" src={ChartSvg} />} 
+                                actions={[<Link to="/Chart/"><Icon type="link" /></Link>,<Icon type="api" onClick={this.handleApi.bind(this,'chart')} />,  <Icon type="ellipsis" />]}>
+                                <Meta title="Chart" description="图表示例" />
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card hoverable  style={{ width: 240 }}
+                                cover={<img alt="example" src={MapSvg} />} 
+                                actions={[<Icon type="api" onClick={this.handleApi.bind(this,'map')} />, <Icon type="link" />,<Icon type="ellipsis" />]}>
+                                <Meta title="Map" description="地图示例" />
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card hoverable  style={{ width: 240 }}
+                                cover={<img alt="example" src={TableSvg} />} 
+                                actions={[<Icon type="api" onClick={this.handleApi.bind(this,'table')} />, <Icon type="link" />,<Icon type="ellipsis" />]}>
+                                <Meta title="Table" description="表格示例" />
+                            </Card>
+                        </Col>
+                    </Row>
+                    
             </Content>
         );
     }

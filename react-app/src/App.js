ef76ef952f26c8059bebb14e8c722f0f1deac8f4
weimin1992 <weimin1992@outlook.com>
2019-07-08 22:24:44 +0800

    待添加面包屑导航

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import zhCN from 'antd/lib/locale-provider/zh_CN';
-import { ConfigProvider,LocaleProvider,Layout, Menu,Icon,Switch,Avatar  } from 'antd';
+import { ConfigProvider,LocaleProvider,Layout, Menu,Icon,Switch,Avatar,Breadcrumb  } from 'antd';
 import Home from './Home';
 import Explore from './Explore';
 // import Chart from './Chart';
@@ -29,6 +29,18 @@ class App extends Component {
   render() {
     const { Header, Footer } = Layout;
     const bgColor=this.state.theme ==='dark'?'#001529':'#fff';
+
+
+    // const breadcrumbNameMap = {
+    //   '/apps': 'Application List',
+    //   '/apps/1': 'Application1',
+    //   '/apps/2': 'Application2',
+    //   '/apps/1/detail': 'Detail',
+    //   '/apps/2/detail': 'Detail',
+    // };
+
+
+
     return (
       <ConfigProvider>
         <LocaleProvider locale={zhCN}>
@@ -49,6 +61,8 @@ class App extends Component {
                     </Menu.Item>
                   </Menu>
                 </Header>
+                
+
                 <Route exact path="/" component={Home} />
                 <Route path="/explore" component={Explore} />
                 <Footer style={{ textAlign: 'center' }}>浪民 ©2019 </Footer>
--- react-app/src/Home/index.js
+++ react-app/src/Home/index.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Layout,Statistic, Card, Row, Col, Icon} from 'antd';
+import { Layout,Statistic, Card, Row, Col, Icon, List, Avatar,} from 'antd';
 
 class Home extends Component {
-
-    onFinish() {
-        console.log('finished!');
-      }
+    
     render() {
         const { Content } = Layout;
-        const { Countdown } = Statistic;
-        const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
-        
+
+        const listData = [];
+        for (let i = 0; i < 23; i++) {
+            listData.push({
+                href: 'http://ant.design',
+                title: `ant design part ${i}`,
+                avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+                content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+            });
+        }
+
+        const IconText = ({ type, text }) => (
+            <span>
+              <Icon type={type} style={{ marginRight: 8 }} />
+              {text}
+            </span>
+        );
+
         return (
             <Content style={{ padding: '0 50px',minHeight: 280,marginTop:80 }}>
-                <Row gutter={16}>
-                    <Col span={12}>
-                        <Card>
-                            <Countdown title="最近一次放假倒计时" value={deadline} onFinish={this.onFinish} />
-                        </Card>
-                    </Col>
-                    <Col span={12}>
-                        <Card>
-                        <Statistic
-                            title="Idle"
-                            value={9.3}
-                            precision={2}
-                            valueStyle={{ color: '#cf1322' }}
-                            prefix={<Icon type="arrow-down" />}
-                            suffix="%"
-                        />
-                        </Card>
-                    </Col>
-                </Row>
+                <List itemLayout="vertical" size="large" 
+                    pagination={{ onChange: page => { console.log(page);  }, pageSize: 3, }}
+                    dataSource={listData}
+                    footer={ <div> <b>ant design</b> footer part </div> }
+                    renderItem={item => ( 
+                        <List.Item key={item.title}
+                            actions={[
+                                <IconText type="star-o" text="156" />,
+                                <IconText type="like-o" text="156" />,
+                                <IconText type="message" text="2" />,
+                            ]}
+                            extra={  <img width={272} alt="logo"  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />
+                         }>
+                        <List.Item.Meta avatar={<Avatar src={item.avatar} />} title={<a href={item.href}>{item.title}</a>} description={item.description}/>
+                        {item.content}
+                     </List.Item>
+                )} />
             </Content>
         );
     }
